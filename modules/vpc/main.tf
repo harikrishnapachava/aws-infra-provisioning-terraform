7d033fae747fa7afde4b432b23a3286f50d700cf
Harikrishna <59684601+harikrishnapachava@users.noreply.github.com>
2025-03-24 16:56:34 +0400

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,69 +1,76 @@
 # This module will create the VPC, public and private subnets, nat gateway, internet gateway, routes, route tables and associate the route tables to subnets
 
 #vpc
-resource "aws_vpc" "production_vpc" {
+resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Name = var.vpc_name
+    Name        = var.vpc_name
+    Environment = var.environment
   }
 }
 
 # pvt subnet
 resource "aws_subnet" "private_subnet" {
-  count            = length(var.private_subnet_cidrs)
-  vpc_id           = aws_vpc.production_vpc.id
-  cidr_block       = var.private_subnet_cidrs[count.index]
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
   availability_zone = element(var.availability_zones, count.index)
   tags = {
-    Name = "private-subnet-${count.index + 1}"
+    Name        = "${var.environment}-private-subnet-${count.index + 1}"
+    Environment = var.environment
   }
 }
 
 # pub subnet
 resource "aws_subnet" "public_subnet" {
-  count            = length(var.public_subnet_cidrs)
-  vpc_id           = aws_vpc.production_vpc.id
-  cidr_block       = var.public_subnet_cidrs[count.index]
+  count             = length(var.public_subnet_cidrs)
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.public_subnet_cidrs[count.index]
   availability_zone = element(var.availability_zones, count.index)
   tags = {
-    Name = "public-subnet-${count.index + 1}"
+    Name        = "${var.environment}-public-subnet-${count.index + 1}"
+    Environment = var.environment
   }
 }
 
 #EIP for NAT GW
 resource "aws_eip" "nat_eip" {
   count = length(var.public_subnet_cidrs) # Matches number of public subnets
-  vpc   = true
+  domain = "vpc"
   tags = {
-    Name = "nat-eip-${count.index + 1}"
+    Name        = "${var.environment}-nat-eip-${count.index + 1}"
+    Environment = var.environment
   }
 }
 
 # NAT GW
 resource "aws_nat_gateway" "nat_gw" {
-  count        = length(var.public_subnet_cidrs) # Matches number of public subnets
+  count         = length(var.public_subnet_cidrs) # Matches number of public subnets
   allocation_id = aws_eip.nat_eip[count.index].id
   subnet_id     = aws_subnet.public_subnet[count.index].id
   tags = {
-    Name = "nat-gw-${count.index + 1}"
+    Name        = "${var.environment}-nat-gw-${count.index + 1}"
+    Environment = var.environment
   }
 }
 
 # IGW
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.production_vpc.id
+  vpc_id = aws_vpc.vpc.id
   tags = {
-    Name = "igw"
+    Name        = "${var.environment}-igw"
+    Environment = var.environment
   }
 }
 
 # RT for pub subnet
 resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.production_vpc.id
+  vpc_id = aws_vpc.vpc.id
   tags = {
-    Name = "public-rt"
+    Name        = "${var.environment}-public-rt"
+    Environment = var.environment
   }
 }
 
@@ -84,18 +91,19 @@ resource "aws_route_table_association" "public_association" {
 # pvt subnet RT
 resource "aws_route_table" "private_rt" {
   count  = length(aws_subnet.private_subnet)
-  vpc_id = aws_vpc.production_vpc.id
+  vpc_id = aws_vpc.vpc.id
   tags = {
-    Name = "private-rt-${count.index + 1}"
+    Name        = "${var.environment}-private-rt-${count.index + 1}"
+    Environment = var.environment
   }
 }
 
 # routes for pvt subnets
 resource "aws_route" "private_route" {
-  count                = length(aws_subnet.private_subnet)
-  route_table_id       = aws_route_table.private_rt[count.index].id
+  count                  = length(aws_subnet.private_subnet)
+  route_table_id         = aws_route_table.private_rt[count.index].id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id       = aws_nat_gateway.nat_gw[count.index].id
+  nat_gateway_id         = aws_nat_gateway.nat_gw[count.index].id
 }
 
 # rt association for pvt subnet
