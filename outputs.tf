3a43bac164a147b64dcc884d1619abc2e57f756b
Harikrishna <59684601+harikrishnapachava@users.noreply.github.com>
2025-03-24 17:05:42 +0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,24 @@
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  value       = module.vpc.vpc_id
+  description = "ID of the created VPC"
 }
 
 output "public_subnet_ids" {
-  value = module.vpc.public_subnet_ids
+  value       = module.vpc.public_subnet_ids
+  description = "List of Public Subnet IDs"
 }
 
 output "private_subnet_ids" {
-  value = module.vpc.private_subnet_ids
+  value       = module.vpc.private_subnet_ids
+  description = "List of Private Subnet IDs"
+}
+
+output "nat_gateway_ids" {
+  value       = module.vpc.nat_gateway_ids
+  description = "List of NAT Gateway IDs"
+}
+
+output "internet_gateway_id" {
+  value       = module.vpc.internet_gateway_id
+  description = "ID of the Internet Gateway"
 }
