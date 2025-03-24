ac08224e59b3d6a07ffd718fc93c60b9db660995
Harikrishna <59684601+harikrishnapachava@users.noreply.github.com>
2025-03-24 17:03:59 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ module "vpc" {
   source               = "./modules/vpc"
   vpc_cidr             = var.vpc_cidr
   vpc_name             = var.vpc_name
-  # Set availability zones based on the environment. For Ex: 2 AZs for production , 1 AZ for staging
+  environment          = var.environment
+  # Set availability zones based on the environment. For Ex: 2 AZs for production, 1 AZ for staging
   availability_zones   = var.environment == "production" ? var.availability_zones : [var.availability_zones[0]]
-  # Set the public and private subnet CIDRs based on the environment. For Ex: 2 AZs for production , 1 AZ for staging
+  # Set the public and private subnet CIDRs based on the environment. For Ex: 2 AZs for production, 1 AZ for staging
   public_subnet_cidrs  = var.environment == "production" ? var.public_subnet_cidrs : [var.public_subnet_cidrs[0]]
   private_subnet_cidrs = var.environment == "production" ? var.private_subnet_cidrs : [var.private_subnet_cidrs[0]]
 }
-
